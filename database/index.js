d180962a725d4aa782130861421608199c8d1ea2
mina <minakazemimanesh@email.arizona.edu>
2018-11-02 16:42:22 -0700

    finilized schema and database functions

--- database/index.js
+++ database/index.js
@@ -1,39 +1,47 @@
-var mysql = require('mysql');
-var con =  mysql.createConnection({
-    user: 'mina',
-    password: '1581338',
-    host: 'localhost',
-    database: 'reviews'
-})
+const mysql = require('mysql');
 
-var saveToUserReviewtable = (data, cb) => {
-    var sql = 'INSERT INTO userReview (username, photo, rateNumber, createdAt, review, roomID) VALUES (?,?,?,?,?,?)';
-    con.query(sql, [data.username, data.photo, data.rateNumber, data.createdAt, data.review, data.roomID], (err) => {
-        if(err) {
-            cb(err, null);
-        }
-    });
-}
+const con = mysql.createConnection({
+  user: 'mina',
+  password: '1581338',
+  host: 'localhost',
+  database: 'reviews',
+});
 
-var saveToRatingtable = (data, cb) => {
-    var sql = 'INSERT INTO rating (numberOfReviews, overallrate, accuracy, communication, cleanliness, location, check_in, value, report) VALUES (?,?,?,?,?,?,?,?,?)';
-    con.query(sql, [data.numberOfReviews, data.overallrate, data.accuracy, data.communication, data.cleanliness, data.location, data.check_in, data.value, data.report], (err) => {
-        if(err) {
-            cb(err, null);
-        }
-    });
-}
+const saveToUserReviewtable = (data, cb) => {
+  const sql = 'INSERT INTO userReview (username, photo, rateNumber, createdAt, review, homeID, report) VALUES (?,?,?,?,?,?,?)';
+  con.query(sql, [data.username, data.photo, data.rateNumber,
+    data.createdAt, data.review, data.homeID, data.report], (err, res) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, res);
+    }
+  });
+};
 
-var getData = (id, cb) => {
-    var sql = `select * from userReview inner join rating on (rating.id=${id} AND userReview.roomID=${id})`;
-    con.query(sql, (err, data) => {
-        if(err) {
-            cb(err, null);
-        } else {
-            cb(null, data);
-        }
-    })
-}
+const saveToRatingtable = (data, cb) => {
+  const sql = 'INSERT INTO rating (numberOfReviews, overallrate, accuracy, communication, cleanliness, location, check_in, value) VALUES (?,?,?,?,?,?,?,?)';
+  con.query(sql, [data.numberOfReviews, data.overallrate, data.accuracy,
+    data.communication, data.cleanliness, data.location, data.check_in,
+    data.value], (err, res) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, res);
+    }
+  });
+};
+
+const getData = (id, cb) => {
+  const sql = `select * from userReview inner join rating on (rating.id=${id} AND userReview.homeID=${id})`;
+  con.query(sql, (err, data) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, data);
+    }
+  });
+};
 
 module.exports.saveToUserReviewtable = saveToUserReviewtable;
 module.exports.saveToRatingtable = saveToRatingtable;
