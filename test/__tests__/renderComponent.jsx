51eb3498f1cf7649925fb1232e76cccdfb4722ac
mina <minakazemimanesh@email.arizona.edu>
2018-11-05 12:14:56 -0800

    fixed testcases

--- test/__tests__/renderComponent.jsx
+++ test/__tests__/renderComponent.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../../client/src/components/app';
+import Reviewapp from '../../client/src/components/app';
 import TopReviewBar from '../../client/src/components/topreviewbar';
 import ButtomReviewPart from '../../client/src/components/buttomreviewpart';
 import Report from '../../client/src/components/report';
 
-describe('<App />', () => {
+describe('<Reviewapp />', () => {
   it('should render correctly with no props', () => {
-    const component = shallow(<App />);
+    const component = shallow(<Reviewapp />);
     expect(component.exists()).toBe(true);
   });
   it('should render a "Report" component', () => {
-    const component = shallow(<App />);
+    const component = shallow(<Reviewapp />);
     expect(component.find(Report).length).toBe(1);
   });
   it('should render a "TopReviewBar" component', () => {
     const data = [1];
-    const component = shallow(<App />);
+    const component = shallow(<Reviewapp />);
     component.setState({ data });
     expect(component.find(TopReviewBar).length).toBe(1);
   });
   it('should render a "ButtomReviewPart" component', () => {
     const data = [1];
-    const component = shallow(<App />);
+    const component = shallow(<Reviewapp />);
     component.setState({ data });
     expect(component.find(ButtomReviewPart).length).toBe(1);
   });
--- test/__tests__/testAppComponent.jsx
+++ test/__tests__/testAppComponent.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../../client/src/components/app';
+import Reviewapp from '../../client/src/components/app';
 
-describe('App component', () => {
+describe('Reviewapp component', () => {
   it('should toggle the state when flag button get clicked', () => {
-    const app = shallow(<App />);
+    const app = shallow(<Reviewapp />);
     expect(app.state().showModal).toBe(false);
   });
 });
 
-describe('App component', () => {
+describe('Reviewapp component', () => {
   test('checking on handleOpenModal function of <App/>', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<Reviewapp />);
     wrapper.instance().handleOpenModal();
   });
 
   test('checking on handleCloseModal function of <App/>', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<Reviewapp />);
     wrapper.instance().handleCloseModal();
   });
 
   test('checking on getData function of <App/>', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<Reviewapp />);
     wrapper.instance().onSearchhandle();
   });
 
   test('checking on handleOpenModal function of <App/>', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<Reviewapp />);
     wrapper.instance().getData();
   });
 
   test('checking on render function of <App/>', () => {
-    const wrapper = shallow(<App />);
+    const wrapper = shallow(<Reviewapp />);
     wrapper.instance().render();
   });
 });
