a71c4ae6ca7384df0495b85f788c3764c1d4789b
mina <minakazemimanesh@email.arizona.edu>
2018-11-06 10:51:34 -0800

    update server file

--- server/server.js
+++ server/server.js
@@ -10,12 +10,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/homes/:homeId/', express.static(`${__dirname}./../client/dist/`));
 
 // Allow CORS for a given endpoint
-const allowCORS = function(res) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-}
+// eslint-disable-next-line func-names
+const allowCORS = function (res) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+};
 
 app.get('/api/homes/:homeID/allReviews', (req, res) => {
+  //console.log("NOWWW", req.get('host'));
   db.getData(req.params.homeID, (err, result) => {
     if (err) {
       throw err;
